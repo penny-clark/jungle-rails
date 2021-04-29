dc84c80d67ec8c22e6faedf44053971f9ad58872
Penny Clark <respect.cactus@gmail.com>
2021-04-29 05:18:09 +0000

    Add AR instance variables to dashboard controller to display stats in dashboard view

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,18 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
   def show
+    @products_count = Product.count
+    @categories_count = Category.count
   end
+
+  # def products_count
+  #   @products_count = Product.count
+  # end
+  # helper_method :products_count
+
+  # def catagories_count
+  #   @catagories_count = Catagory.all.count
+  # end
+  # helper_method :catagories_count
+
 end
