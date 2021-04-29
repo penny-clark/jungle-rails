b0bd49e0bdc5d86379c10eb84de09e2825122b08
Penny Clark <respect.cactus@gmail.com>
2021-04-29 20:19:47 +0000

    Add routes for register and create users.

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,9 @@ Rails.application.routes.draw do
 
   resources :about, only: [:index]
 
+  get '/register' => 'users#new'
+  get 'users' => 'users#create'
+    
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
