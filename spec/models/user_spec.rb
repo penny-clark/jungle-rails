7200f09aca027bec295f0518ce6ec8b6431b4423
Penny Clark <respect.cactus@gmail.com>
2021-05-04 02:40:50 +0000

    Add validation criteria to user model to pass basic validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,22 +13,70 @@ RSpec.describe User, type: :model do
       expect(user).to be_valid
     end
 
-    # xit "is not valid without first name" do
-    # end
+    it "is not valid without first name" do
+      user = User.new(
+        first_name: nil,
+        last_name: 'Eggerson',
+        email: 'eggy@eggy.egg',
+        password: 'Egg',
+        password_confirmation: 'Egg'
+      )
+      expect(user).to_not be_valid
+    end
 
-    # xit "is not valid without last name" do
-    # end
+    it "is not valid without last name" do
+      user = User.new(
+        first_name: 'Egg',
+        last_name: nil,
+        email: 'eggy@eggy.egg',
+        password: 'Egg',
+        password_confirmation: 'Egg'
+      )
+      expect(user).to_not be_valid
+    end
 
-    # xit "is not valid without email" do
-    # end 
+    it "is not valid without email" do
+      user = User.new(
+        first_name: 'Egg',
+        last_name: 'Eggerson',
+        email: nil,
+        password: 'Egg',
+        password_confirmation: 'Egg'
+      )
+      expect(user).to_not be_valid
+    end 
 
-    # xit "is not valid without password field" do
-    # end
+    it "is not valid without password field" do
+      user = User.new(
+        first_name: 'Egg',
+        last_name: 'Eggerson',
+        email: 'eggy@eggy.egg',
+        password: nil,
+        password_confirmation: 'Egg'
+      )
+      expect(user).to_not be_valid
+    end
 
-    # xit "is not valid without password_confirmation field" do
-    # end
+    it "is not valid without password_confirmation field" do
+      user = User.new(
+        first_name: 'Egg',
+        last_name: 'Eggerson',
+        email: 'eggy@eggy.egg',
+        password: 'Egg',
+        password_confirmation: nil
+      )
+      expect(user).to_not be_valid
+    end
 
     # xit "is not valid if password and passwor confirmation fields don't match" do
+      # user = User.new(
+      #     first_name: 'Egg',
+      #     last_name: 'Eggerson',
+      #     email: 'eggy@eggy.egg',
+      #     password: 'Egg',
+      #     password_confirmation: 'Egg'
+      #   )
+      #   expect(user).to_not be_valid
     # end
 
     # xit "should not be valid if email already exists in database" do
