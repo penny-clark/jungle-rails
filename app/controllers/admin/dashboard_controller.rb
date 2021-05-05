03820224ae6373c8fd6fd7349ceb873094e010f3
Penny Clark <respect.cactus@gmail.com>
2021-05-05 20:20:40 +0000

    Refactor admin authentication from each individiual admin controller to admin base controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,18 +1,7 @@
-class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+class Admin::DashboardController < Admin::BaseController
   def show
     @products_count = Product.count
     @categories_count = Category.count
   end
-
-  # def products_count
-  #   @products_count = Product.count
-  # end
-  # helper_method :products_count
-
-  # def catagories_count
-  #   @catagories_count = Catagory.all.count
-  # end
-  # helper_method :catagories_count
 
 end
